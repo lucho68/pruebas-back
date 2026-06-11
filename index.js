179c8acaa84f1275ee108910baffa17b19646f12
lucho68 <busca2011@gmail.com>
2026-06-11 14:00:13 -0300

    Agrego variable entorno con el puerto

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+require("dotenv").config()
+
 const http= require("http")
 
 function requestController()
 {
 // Logica de la función
-// console.log({global})
-console.log("Request xxx pruebita total!!!!")
+console.log("Llegó 1 Request !")
 }
 
 //Configuro el servidor
 
 const server = http.createServer(requestController)
-server.listen(4005)
+const PORT=process.env.PORT
+
+server.listen(PORT, function(){
+    console.log("La App corre en el puerto: " + PORT)
+})
